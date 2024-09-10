841f2e24e60dc809f6e39c1567f164c6f04a3d1b
kristel2706 <124943579+kristel2706@users.noreply.github.com>
2024-09-10 20:15:01 +0000

    html

--- web.py
+++ web.py
@@ -1,10 +1,9 @@
+#@@ -1,28 +1,44 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
-
 class WebRequestHandler(BaseHTTPRequestHandler):
     def url(self):
         return urlparse(self.path)
-
     def query_data(self):
         return dict(parse_qsl(self.url().query))
 
@@ -17,17 +16,45 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     def get_response(self):
         return f"""
     <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()} </p>
-    <p> Path Original: {self.path} </p>
-    <p> Headers: {self.headers} </p>
-    <p> Query: {self.query_data()} </p>
+    <p> URL Parse Result : {self.url()}         </p>
+    <p> Path Original: {self.path}         </p>
+    <p> Headers: {self.headers}      </p>
+    <p> Query: {self.query_data()}   </p>
+"""
+def do_GET(self):
+
+    host = self.headers.get('Host')
+    user_agent = self.headers.get('User-Agent')
+
+
+    self.send_response(200)
+
+
+    self.send_header("Content-Type", "text/html")
+    self.send_header("Server", "CustomPythonServer")
+    self.send_header("Date", self.date_time_string())
+    self.end_headers()
+
+
+    self.wfile.write(self.get_response(host, user_agent).encode("utf-8"))
+
+def get_response(self, host, user_agent):
+
+    query_data = self.query_data()
+    autor = query_data.get('autor', 'desconocido')
+
+
+    return f"""
+    <h1>Proyecto: {self.url().path} Autor: {autor}</h1>
+    <p> Host: {host} </p>
+    <p> User-Agent: {user_agent} </p>
     """
 
+
+
 if __name__ == "__main__":
-    host = "localhost"
-    port = 8000  # Cambiamos el puerto a 8000
-    server = HTTPServer((host, port), WebRequestHandler)
-    print(f"Starting server on {host}:{port}")  # Mensaje para indicar en qué puerto está escuchando
+    print("Starting server on port 8000")
+    server = HTTPServer(("localhost", 8000), WebRequestHandler) #SE CAMBIO AL PUERTO 8000 
     server.serve_forever()
 
 
