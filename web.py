33429c8dc8610b77515689e5c700307f4dabb73d
kristel2706 <124943579+kristel2706@users.noreply.github.com>
2024-09-10 20:22:57 +0000

    pagina principal

--- web.py
+++ web.py
@@ -8,42 +8,38 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
 
-    def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
-"""
-def do_GET(self):
-
-    host = self.headers.get('Host')
-    user_agent = self.headers.get('User-Agent')
+         host = self.headers.get('Host')
+         user_agent = self.headers.get('User-Agent')
 
 
-    self.send_response(200)
+         self.send_response(200)
+         if self.path == "/":
+
+             with open("home.html", "r") as file:
+              content = file.read()
+
+             self.send_response(200)
+             self.send_header("Content-Type", "text/html")
+             self.end_headers()
+             self.wfile.write(content.encode("utf-8"))
+         else:
+           self.send_response(404)
+           self.send_header("Content-Type", "text/html")
+           self.end_headers()
+           self.wfile.write("<h1>ERROR 404:Not Found</h1>".encode("utf-8"))
 
 
-    self.send_header("Content-Type", "text/html")
-    self.send_header("Server", "CustomPythonServer")
-    self.send_header("Date", self.date_time_string())
-    self.end_headers()
-
-
-    self.wfile.write(self.get_response(host, user_agent).encode("utf-8"))
+         self.send_header("Content-Type", "text/html")
+         self.send_header("Server", "CustomPythonServer")
+         self.send_header("Date", self.date_time_string())
+         self.end_headers()
+         self.wfile.write(self.get_response(host, user_agent).encode("utf-8"))
 
 def get_response(self, host, user_agent):
 
     query_data = self.query_data()
     autor = query_data.get('autor', 'desconocido')
-
-
     return f"""
     <h1>Proyecto: {self.url().path} Autor: {autor}</h1>
     <p> Host: {host} </p>
